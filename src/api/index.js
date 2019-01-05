306098c5358014de56d22004f3ece6a624cc0a1c
Siyuan Ma <sma232@wisc.edu>
2019-01-05 11:17:24 -0600

    Created for heroku

---
+++ src/api/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+const messages = require('./messages');
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.json({
+        message: 'API - 👋🌎🌍🌏'
+    });
+   
+});
+
+
+router.use('/messages', messages);
+
+module.exports = router;
